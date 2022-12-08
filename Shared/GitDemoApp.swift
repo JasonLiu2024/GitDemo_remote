16ff6d469a9430089311a5d8255f36495370bce3
Jason Liu <jasonl@wustl.edu>
2022-12-07 21:21:39 -0600

    Initial Commit

---
+++ Shared/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  Shared
+//
+//  Created by Xiaoyi Liu on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
